c57d68bdfc425bff94b9e0083e0fb019644313db
paridhi089 <70331800+paridhi089@users.noreply.github.com>
2020-09-28 16:24:11 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@
 #     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
 #    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
 #   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
-#  Arch Linux Post Install Setup and Config
+#  Arch Linux PRE Install Setup and Config
 #-------------------------------------------------------------------------
 
 echo "-------------------------------------------------"
@@ -13,7 +13,7 @@ echo "-------------------------------------------------"
 timedatectl set-ntp true
 pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://www.archlinux.org/mirrorlist/?country=IN&protocol=http&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
 
 
